c783adc1bb3745cdd78a4abc5bd86a480a44cf40
Hamcha <victor.hamcha@gmail.com>
2020-04-06 14:55:55 +0200

    fichier projet

---
+++ Jam3DIIM/Assets/Prefab/Player3Drb/RigidBodyMouvement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyMouvement : MonoBehaviour
+{
+  
+    public float speed = 12f;
+    public float jumpHeight = 10f;
+
+    private Rigidbody _rb;
+    private CapsuleCollider col;
+
+    bool isGrounded = true;
+
+    public LayerMask groundMask;
+
+   
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z);
+        //Vector3 move = new Vector3(x, 0, z);
+        //_rb.AddForce(move*speed,ForceMode.VelocityChange); //Voiture
+        // _rb.velocity = move * speed;
+        _rb.MovePosition(transform.position + (move * speed * Time.deltaTime));
+
+        if (Input.GetButton("Jump")&&isGrounded)
+        {
+            _rb.AddForce(Vector3.up * jumpHeight,ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+       
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * 0.9f, groundMask);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.tag=="platforme")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "platforme")
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Jam3DIIM/Assets/Prefab/Player3Drb/ThreeDPlayerLooking.cs
@@ -0,0 +1,100 @@
+﻿
+//// temple Run //
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ThreeDPlayerLooking : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    public float mouseSnesy = 0.100f;
+//    public Transform playerBody;
+//    private float xRotation = 0f;
+//    bool rotateR = false;
+//    bool CanRotate;
+//    Vector3 changeAngle;
+//    float turnAngle;
+//    public bool canRotate = false;
+//    void Start()
+//    {
+//        Cursor.lockState = CursorLockMode.Locked;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //float mouseX = Input.GetAxis("Mouse X")*mouseSnesy;
+//        //float mouseX = Input.GetAxis("Mouse X") * mouseSnesy;
+//        float mouseY = Input.GetAxis("Mouse Y") * mouseSnesy;
+
+//        xRotation -= mouseY;
+//        // xRotation = Mathf.Clamp(xRotation, -90f,90f);
+
+//        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+//        if (!rotateR)
+//        {
+//            if(Input.GetKeyDown(KeyCode.RightArrow))
+//            {
+//                turnAngle = 90f;
+//                rotateR = true;
+//                changeAngle = playerBody.transform.eulerAngles;
+//            }
+//            else if(Input.GetKeyDown(KeyCode.LeftArrow))
+//            {
+//                turnAngle = -90f;
+//                rotateR = true;
+//                changeAngle = playerBody.transform.eulerAngles;
+//            }
+
+
+//        }
+//        if (rotateR)
+//        {
+//            Vector3 to = new Vector3(0, changeAngle.y+90, 0);
+//            Vector3 too = new Vector3(0, changeAngle.y-90, 0);
+//            if (Vector3.Distance(transform.eulerAngles, too) > 0.01f)
+//            {
+//                playerBody.transform.eulerAngles += new Vector3(0, -1, 0);
+//                //playerBody.transform.eulerAngles += -Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime*mouseSnesy);
+//            }
+//            else
+//            {
+//                playerBody.transform.eulerAngles = to;
+//                rotateR = false;
+//                Debug.Log("mlkj");
+//            }
+
+//        }
+//    }
+//}
+
+//NORMAL//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDPlayerLooking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float mouseSnesy = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSnesy;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSnesy;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+
